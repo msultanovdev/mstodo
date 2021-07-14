cde98218d3f40e32ece18c730a529d37c4946798
msultanovdev <mukhammadamin.sultanov@gmail.com>
2021-07-14 06:09:07 +0500

    added tasks

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -36,11 +36,11 @@ const Tasks = ({ id, list, onEditTitle, onAddTask, withoutEmpty, onRemoveTask, o
                                 <div className="tasks__items-checkbox">
                                     <input
                                         onChange={completed}
-                                        id={`task-${id}`}
+                                        id={task.id}
                                         type="checkbox"
                                         checked={completed}
                                     />
-                                    <label htmlFor={`task-${id}`}>
+                                    <label htmlFor={task.id}>
                                         <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                                         </svg>
